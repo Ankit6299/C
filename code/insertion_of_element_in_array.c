402ead952b9aa64b570cf5b56aac7b1f3daa7fe1
Ankit <ankit6299659@gmail.com>
2024-08-20 21:32:38 +0530

    e

---
+++ code/insertion_of_element_in_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+    int arr[5] = {27,7,2,1,59};
+    int position = 3;
+    int element = 30;
+
+    for (int i=0;i<5;i++) {
+        if (arr[i] = position){
+            arr[i] = element
+        }
+    }
+}
